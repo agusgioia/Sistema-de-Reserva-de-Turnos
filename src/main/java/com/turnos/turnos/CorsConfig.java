fe9d40b06e56cbae3417c36bb4efe9c1089344a9
agusgioia <131310437+agusgioia@users.noreply.github.com>
2026-03-06 15:55:48 -0300

    Add additional allowed origin for CORS

--- src/main/java/com/turnos/turnos/CorsConfig.java
+++ src/main/java/com/turnos/turnos/CorsConfig.java
@@ -17,11 +17,12 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://sistema-de-reserva-de-turnos.onrender.com")
                         .allowedMethods("*")
                         .allowedHeaders("*");
 
             }
         };
     }
+
 }
